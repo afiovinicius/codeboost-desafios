5bd5be0a5aeefbad88e40b7a5700fabcdf35d17f
afiovinicius <afiovinicius@gmail.com>
2022-10-08 15:13:54 -0300

    upload dashboard

--- Codeboost_Scripts/main.js
+++ Codeboost_Scripts/main.js
@@ -1,1 +1,14 @@
- 
+// Campo de senha (para telas de login ou cadastro)
+const btnEye = document.querySelector(".btn-eye");
+const inputPasswd = document.querySelector("#i-passwd");
+const iconPasswd = document.querySelector(".eye-passwd");
+
+btnEye.addEventListener("click", () => {
+  if (inputPasswd.getAttribute("type") === "password") {
+    inputPasswd.setAttribute("type", "text");
+    iconPasswd.setAttribute("src", "./eye.svg");
+  } else {
+    inputPasswd.setAttribute("type", "password");
+    iconPasswd.setAttribute("src", "./eye-slash.svg");
+  }
+});
